69cf7eeefaadab4ad39803e6d5f9d4923409dc5c
Phước Cao <caophuoc6@gmail.com>
2022-10-20 11:43:37 +0700

    add view for user list

---
+++ 7.3.0/src/Manage_Intern_Version_2.Web/Models/User/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Manage_Intern_Version_2.Users.Dtos;
+using System.Collections.Generic;
+
+namespace Manage_Intern_Version_2.Web.Models.User
+{
+    public class UserViewModel
+    {
+        public IReadOnlyList<UserListDto> Users { get; }
+        public UserViewModel (IReadOnlyList<UserListDto> users)
+        {
+            Users = users;
+        }
+
+    }
+}
