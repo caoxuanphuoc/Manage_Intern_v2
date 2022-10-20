babe7e3656255c63cbdc938b296c40e9477896f1
Phước Cao <caophuoc6@gmail.com>
2022-10-20 08:40:30 +0700

    add model user, intern, mentor

---
+++ 7.3.0/src/Manage_Intern_Version_2.Core/Interns/Intern.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Domain.Entities;
+using Abp.Timing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Manage_Intern_Version_2.Users;
+
+namespace Manage_Intern_Version_2.Interns
+{
+    [Table("Intern")]
+    public class Intern : Entity
+    {
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string StudentCode { get; set; }
+        public School School { get; set; }
+        [MaxLength(50)]
+        public string Department { get; set; }
+        public DateTime StartDay { get; set; }
+        public DateTime EndDay { get; set; }
+
+        public Intern()
+        {
+            StartDay = Clock.Now;
+        }
+    }
+}
---
+++ 7.3.0/src/Manage_Intern_Version_2.Core/Mentors/Mentor.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Manage_Intern_Version_2.Users;
+
+namespace Manage_Intern_Version_2.Mentors
+{
+    [Table("Mentor")]
+    public class Mentor : Entity
+    {
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string EmployeeCode { get; set; }
+        [MaxLength(50)]
+        public string DepartMent { get; set; }
+        public Mentor(string employeeCode, string departMent)
+        {
+            EmployeeCode = employeeCode;
+            DepartMent = departMent;
+        }
+    }
+}
